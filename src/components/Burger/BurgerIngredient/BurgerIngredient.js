daec6a80c7e1ef091a98f2ae0427753da1a91dc7
plamenyovchev <plamenkooo@gmail.com>
2019-08-10 09:41:02 +0300

    implement BurgerIngredient component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import classes from './BurgerIngredient.css';
+
 const burgerIngredient = (props) => {
-    return (
-        <div></div>
-    );
+    let ingredient = null;
+    if (ingredientTypeToCssClass[props.ingredientType]) {
+        const ingredientType = ingredientTypeToCssClass[props.ingredientType];
+        if (props.ingredientType !== 'bread-top') {
+            ingredient = <div className={ingredientType}></div>;
+        } else {
+            ingredient = (
+                <div className={ingredientType}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+        }
+        ingredient = ingredientTypeToCssClass[props.ingredientType];
+    }
+
+    return ingredient;
 }
+
+const ingredientTypeToCssClass = {
+    "bread-bottom": classes.BreadBottom,
+    "bread-top": classes.BreadTop,
+    "meat": classes.Meat,
+    "cheese": classes.Cheese,
+    "bacon": classes.Bacon,
+    "salad": classes.Salad
+};
 
 export default burgerIngredient;
