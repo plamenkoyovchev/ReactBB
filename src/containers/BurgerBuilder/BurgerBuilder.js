54a8e07b1feedb9148190aa318e48da7a2fc761d
plamenyovchev <plamenkooo@gmail.com>
2019-08-10 11:32:15 +0300

    Dynamically read ingredients from burge builder state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,12 +4,23 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                cheese: 2,
+                bacon: 1,
+                salad: 1,
+                meat: 2
+            }
+        };
+    }
 
 
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger controls</div>
             </Aux>
         );
