051b1d1ed404b22ff01fc389073cc249c2a53a9c
plamenyovchev <plamenkooo@gmail.com>
2019-10-16 21:46:16 +0300

    Create Redux reducer and store and use it in the App

---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    }
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
