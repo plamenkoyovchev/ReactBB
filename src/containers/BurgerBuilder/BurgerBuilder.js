0fa3db744391211ac9c014c0bd8707220d5e85b5
plamenyovchev <plamenkooo@gmail.com>
2019-10-17 08:46:02 +0300

    Remove passing of query parameters to checkout page since we are using redux store for state management

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -53,17 +53,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for (let key in this.props.ingredients) {
-            queryParams.push(`${encodeURIComponent(key)}=${encodeURIComponent(this.props.ingredients[key])}`);
-        }
-
-        queryParams.push(`price=${this.props.totalPrice}`);
-
-        this.props.history.push({
-            pathname: '/checkout',
-            search: queryParams.join('&')
-        });
+        this.props.history.push('/checkout');
     }
 
     render() {
