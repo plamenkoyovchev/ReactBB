90dc44b68f4d47b5db73c377ee332691ad426902
plamenyovchev <plamenkooo@gmail.com>
2019-10-19 19:54:27 +0300

    install & configure redux-thunk for async operations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
@@ -12,7 +12,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app =
     <Provider store={store}>
