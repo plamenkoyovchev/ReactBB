3675306cf0bb91c27b9f02c3c843d2400b93b5e6
Plamen Yovchev <plamenkooo@gmail.com>
2019-10-31 19:38:17 +0200

    Refactor auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../../shared/utility';
 
 const initialState = {
     loading: false,
@@ -10,42 +11,51 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.AUTH_START:
-            return {
-                ...state,
-                loading: true,
-                error: null
-            };
-        case actionTypes.AUTH_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                token: action.token,
-                userId: action.userId,
-                error: null
-            };
-        case actionTypes.AUTH_FAIL:
-            return {
-                ...state,
-                loading: false,
-                error: action.error
-            };
-        case actionTypes.AUTH_LOGOUT:
-            return {
-                ...state,
-                loading: false,
-                error: null,
-                userId: null,
-                token: null
-            };
-        case actionTypes.SET_AUTH_REDIRECT_PATH:
-            return {
-                ...state,
-                authRedirectPath: action.path
-            };
-        default:
-            return state;
+        case actionTypes.AUTH_START: return authStart(state);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state);
+        case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action);
+        default: return state;
     }
+};
+
+const authStart = state => {
+    return updateObject(state, {
+        loading: true, 
+        error: null 
+    });
+};
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        loading: false,
+        token: action.token,
+        userId: action.userId,
+        error: null
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        loading: false,
+        error: action.error
+    });
+};
+
+const authLogout = state => {
+    return updateObject(state, {
+        loading: false,
+        error: null,
+        userId: null,
+        token: null
+    });
+};
+
+const setAuthRedirectPath = (state, action) => {
+    return updateObject(state, {
+        authRedirectPath: action.path
+    });
 };
 
 export default reducer;
