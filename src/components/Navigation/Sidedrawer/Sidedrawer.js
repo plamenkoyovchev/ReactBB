f354477bc80724a8139910a021ea90a14f2aa895
plamenyovchev <plamenkooo@gmail.com>
2019-10-31 07:22:20 +0200

    Close sidedrawer when user clicks on it

--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -17,7 +17,7 @@ const sidedrawer = (props) => {
     return (
         <Auxilary>
             <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
