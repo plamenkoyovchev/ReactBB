bfb04be077d5e3430da064e32edd11f43492314a
plamenyovchev <plamenkooo@gmail.com>
2019-11-02 15:51:28 +0200

    Test NavigationItems length when user is authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,17 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should have three <NavigationItem /> elements if is authenticated', () => {
+        wrapper.setProps({ isLoggedIn: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
